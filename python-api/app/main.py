11399ca287d9455324bf5e51ff6a3ba971839a10
JoeyZ56 <jzazzi56@gmail.com>
2025-04-03 22:04:24 -0700

    created post route testing minimul summary form

--- python-api/app/main.py
+++ python-api/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -15,3 +16,13 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI"}
+
+class NoteRequest(BaseModel):
+    text: str
+
+@app.post("/summarize")
+def summarize_note(note: NoteRequest):
+    #Temp dummy notes
+    return {
+        "Summary": f"Summary of: {note.text[:50]}..."
+    }
